00bffe51a6b29948a342434b24528a540a4d69bf
Mathias Hansen <me@codemonkey.io>
2014-06-19 16:09:54 -0400

    Improved geocoding caching

--- consumer/lib/Consumer/Consumer.php
+++ consumer/lib/Consumer/Consumer.php
@@ -3,14 +3,35 @@ namespace Consumer;
 
 abstract class Consumer
 {
-	abstract function consume($filter);
+    abstract function consume($filter);
 
-	public function geocode($address)
+    public function geocode($address)
     {
+        $notFound = false;
         $address = trim($address);
 
         if (empty($address)) {
             return NULL;
+        }
+
+        // If address is a coordinate
+        if (substr($address, 0, 3) == 'ÜT:') {
+            $address = trim(substr($address, 4));
+            list($lat, $lon) = explode(',', $address);
+
+            $result = $this->reverse_geocode($lat, $lon);
+            $result['lat'] = $lat;
+            $result['lon'] = $lon;
+
+            return $result;
+        }
+
+        // Look up in cache first
+        $m = new \Memcached();
+        $m->addServer('localhost', 11211);
+
+        if ($result = $m->get(sha1($address)) && $m->getResultCode() != \Memcached::RES_NOTFOUND) {
+            return $result;
         }
 
         if (GEOCODING_SERVICE == 'GOOGLE') {
@@ -23,7 +44,7 @@ abstract class Consumer
             $data = json_decode($content);
 
             if ($data->status != 'OK' || count($data->results) <= 0)
-                return NULL;
+                $notFound = true;
 
             $result = $data->results[0];
 
@@ -59,7 +80,7 @@ abstract class Consumer
             $data = json_decode($content);
 
             if (!isset($data->results) || count($data->results) <= 0)
-                return NULL;
+                $notFound = true;
 
             $result = $data->results[0];
 
@@ -78,19 +99,35 @@ abstract class Consumer
         }
 
         // Set result
-        $result = array(
-            'lat' => $lat,
-            'lon' => $lon,
-            'state' => $state,
-            'county' => $county,
-            'country' => $country,
-        );
+        if (!$notFound) {
+            $result = array(
+                'lat' => $lat,
+                'lon' => $lon,
+                'state' => $state,
+                'county' => $county,
+                'country' => $country,
+            );
+        }
+
+        // Save in cache
+        $m->set(sha1($address), $result);
+        var_dump($m->get(sha1($address)));
 
         return $result;
     }
 
     public function reverse_geocode($lat, $lon)
     {
+        $notFound = false;
+
+        // Look up in cache first
+        $m = new \Memcached();
+        $m->addServer('localhost', 11211);
+
+        if ($result = $m->get(sha1($lat . $lon)) && $m->getResultCode() != \Memcached::RES_NOTFOUND) {
+            return $result;
+        }
+
         if (GEOCODING_SERVICE == 'GOOGLE') {
             $url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='. $lat .','. $lon .'&sensor=false&key=' . urlencode(GOOGLE_API_KEY);
             $content = @file_get_contents($url);
@@ -101,7 +138,7 @@ abstract class Consumer
             $data = json_decode($content);
 
             if ($data->status != 'OK' || count($data->results) <= 0)
-                return NULL;
+                $notFound = true;
 
             $result = $data->results[0];
 
@@ -121,7 +158,7 @@ abstract class Consumer
             }
             
             // Return result
-            return array(
+            $result = array(
                 'state' => $state,
                 'county' => $county,
                 'country' => $country,
@@ -140,12 +177,12 @@ abstract class Consumer
             $data = json_decode($content);
 
             if (!isset($data->results) || count($data->results) <= 0)
-                return NULL;
+                $notFound = true;
 
             $result = $data->results[0];
 
             // Return result
-            return array(
+            $result = array(
                 'state' => $result->address_components->state,
                 'county' => isset($result->address_components->county) ? $result->address_components->county : '',
                 'country' => 'USA',
@@ -155,5 +192,11 @@ abstract class Consumer
         {
             throw new Exception('Invalid geocoding service: ' . GEOCODING_SERVICE);
         }
+
+        // Save in cache
+        $m->set(sha1($lat . $lon), $result);
+        print_r($result);
+
+        return $result;
     }
 }
