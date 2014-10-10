1643fe0a8863a941033fb24d1b122b87f9b2df3a
Mike Calvo <mike@engagedc.com>
2014-10-10 13:24:08 -0400

    set footer location

--- website/application/views/layouts/default_layout.php
+++ website/application/views/layouts/default_layout.php
@@ -6,56 +6,61 @@
 <?php if (empty($body_id)) $body_id = NULL; ?>
 <body id="<?php echo $body_id; ?>">
 
-<div class="container wrapper">
+<div class="wrapper">
   <header>
-    <a class="logo" href="/">ElectionDesk</a>
-
-    <nav>
-      <ul>
-        <li class="active"><a href="/">Home</a></li>
-        <?php
-				if ($this->tank_auth->is_logged_in()):
-				?>
-        <li><a href="/conversations">Conversations</a></li>
-        <li><a href="/settings">Settings</a></li>
-        <li><a href="/auth/logout">Logout</a></li>
-        <li class="btn btn-primary"><a href="/post">Post</a></li>
-        <?php
-        else:
-        ?>
-	    	<li><a href="/help">Help</a></li>
-		    <?php
-        endif;
-        ?>
-      </ul>
-    </nav>
+    <div class="container">
+      <a class="logo" href="/">ElectionDesk</a>
+      <nav>
+        <ul>
+          <li class="active"><a href="/">Home</a></li>
+          <?php
+  				if ($this->tank_auth->is_logged_in()):
+  				?>
+          <li><a href="/conversations">Conversations</a></li>
+          <li><a href="/settings">Settings</a></li>
+          <li><a href="/auth/logout">Logout</a></li>
+          <li class="btn btn-primary"><a href="/post">Post</a></li>
+          <?php
+          else:
+          ?>
+  	    	<li><a href="/help">Help</a></li>
+  		    <?php
+          endif;
+          ?>
+        </ul>
+      </nav>
+    </div><!--/container-->
   </header>
 
 	<main>
-		<?php echo $content; ?>
+    <div class="container">
+      <?php echo $content; ?>
+    </div><!--/container-->
 	</main>
-</div>
 
-<footer>
-  <div class="container">
-    <img src="/assets/img/vip-logo.png" alt="Voting Information Project">
+  <footer>
+    <div class="container">
+      <img src="/assets/img/vip-logo.png" alt="Voting Information Project">
 
-    <nav>
-      <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/help">Help</a></li>
-        <li><a href="/glossary">Glossary</a></li>
-        <li><a href="/terms-of-use">Terms of Use</a></li>
-        <li><a href="/privacy-policy">Privacy Policy</a></li>
-        <?php if ($this->session->userdata('is_admin') == 1) : ?>
-				<li><a href="/admin">Admin</a></li>
-				<?php endif; ?>
-      </ul>
-    </nav>
+      <nav>
+        <ul>
+          <li><a href="/about">About</a></li>
+          <li><a href="/help">Help</a></li>
+          <li><a href="/glossary">Glossary</a></li>
+          <li><a href="/terms-of-use">Terms of Use</a></li>
+          <li><a href="/privacy-policy">Privacy Policy</a></li>
+          <?php if ($this->session->userdata('is_admin') == 1) : ?>
+          <li><a href="/admin">Admin</a></li>
+          <?php endif; ?>
+        </ul>
+      </nav>
 
-    <span>&copy; Copyright 2012-<?php echo date('Y'); ?> Voting Information Project. All Rights Reserved.</span>
-  </div>
-</footer>
+      <span>&copy; Copyright 2012-<?php echo date('Y'); ?> Voting Information Project. All Rights Reserved.</span>
+    </div>
+  </footer>
+</div><!--/wrapper-->
+
+
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-rc.0/angular.min.js"></script>
