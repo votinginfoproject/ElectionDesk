0cd3597e47139c4e88d1e13b39263ddb2b76caa9
Mike Calvo <mike@engagedc.com>
2014-10-14 14:01:36 -0400

    modal reply styles added

--- website/application/views/slices/modal_reply.php
+++ website/application/views/slices/modal_reply.php
@@ -1,12 +1,15 @@
 <script type="text/ng-template" id="replyModalContent.html">
+  <div class="modal-top"></div>
   <div class="modal-body">
-    <p>Reply from <select name="accounts" id="accountswitcher">
+    <p class="from">Reply from</p>
+
+    <select name="accounts" id="accountswitcher" class="form-control">
     <?php foreach ($accounts as $account): ?>
       <option value="<?php echo $account->id; ?>"<?php if ($account->is_primary == 1) echo ' selected="selected"' ?>>@<?php echo $account->name; ?></option>
     <?php endforeach; ?>
-    </select></p>
+    </select>
 
-    <h2>Reply to <span class="username">@{{ interaction.interaction.author.username }}</span></h2>
+    <p class="to">Reply to <span class="username">@{{ interaction.interaction.author.username }}</span></p>
     <span class="error"></span>
 
     <?php
@@ -25,8 +28,8 @@
           <p class="character-count">{{ twitterMessage.length }}/140 characters</p>
         </div>
 
-        <div class="form-group">
-          <button class="btn btn-warning" ng-click="cancel()">Cancel</button>
+        <div class="form-group button-wrapper">
+          <button class="btn btn-danger" ng-click="cancel()">Cancel</button>
           <input type="submit" class="btn btn-primary" value="Reply">
         </div>
       </form>
@@ -34,4 +37,5 @@
     endif;
     ?>
     </div>
+    <div class="modal-bottom"></div>
 </script>
