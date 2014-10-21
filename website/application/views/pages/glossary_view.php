b531d910dfd8f00fd006fcc7d8a50e156c8fb572
Mike Calvo <mike@engagedc.com>
2014-10-21 11:31:05 -0400

    updated heading tags

--- website/application/views/pages/glossary_view.php
+++ website/application/views/pages/glossary_view.php
@@ -1,47 +1,47 @@
 <h1>Glossary</h1>
 
-<h2>What is an app?</h2>
+<h3>What is an app?</h3>
 <p>Popularized in the general lexicon by the iPhone, an app is simply an application that performs a specific function on your computer or handheld device. Apps run the gamut from Web browsers and games to specialized programs like digital recorders, online chat or music players. (See Wikipedia entry.)</p>
 
-<h2>What is Facebook?</h2>
+<h3>What is Facebook?</h3>
 <p>Facebook is the most popular social networking site in the world, with more than 200 million members. Members’ home page streams can now be seen in a wide range of applications and devices. (See Wikipedia entry.)</p>
 
-<h2>What is a feed?</h2>
+<h3>What is a feed?</h3>
 <p>A Web feed or RSS feed is a format that provides users with frequently updated content. Content distributors syndicate a Web feed, enabling users to subscribe to a site’s latest content. By using a news reader to subscribe to a feed, you can read the latest posts or watch the newest videos on your computer or portable device on your own schedule. (See Wikipedia entry.)</p>
 
-<h2>What is geotagging?</h2>
+<h3>What is geotagging?</h3>
 <p>Geotagging is the process of aping location-based metadata to media such as photos, video or online maps. Geotagging can help users find a wide variety of businesses and services based on location. (See Wikipedia entry.)</p>
 
-<h2>What is Government 2.0?</h2>
+<h3>What is Government 2.0?</h3>
 <p>Government 2.0 is the term for attempts to apply the social networking and integration advantages of Web 2.0 to the practice of government.</p>
 
-<h2>What is a hashtag?</h2>
+<h3>What is a hashtag?</h3>
 <p>A hashtag (or hash tag) is a community-driven convention for aping apitional context and metadata to your tweets. Similar to tags on Flickr, you ap them in-line to your Twitter posts by prefixing a word with a hash symbol (or number sign). Twitter users often use a hashtag like #followfridayto aggregate, organize and discover relevant posts.</p>
 
-<h2>What is hosting?</h2>
+<h3>What is hosting?</h3>
 <p>A blog, video or podcast needs a hosting service before it can appear online. Companies sometimes host their blogs on their own servers, but a better choice for video or audio is to use a host such as YouTube, Vipler orMagnify.net for video and a host such as Libsyn for podcasts. (See Wikipediafor different kinds of hosting.)</p>
 
-<h2>What is social media?</h2>
+<h3>What is social media?</h3>
 <p>Social media are works of user-created video, audio, text or multimedia that are published and shared in a social environment, such as a blog, podcast, forum, wiki or video hosting site. More broadly, social media refers to any online technology that lets people publish, converse and share content online. (See Wikipedia entry.)</p>
 
-<h2>What is a tag cloud?</h2>
+<h3>What is a tag cloud?</h3>
 <p>A tag cloud is a visual representation of the popularity of the tags or descriptions that people are using on a blog or website. Popular tags are often shown in a large type and less popular tags in smaller type. (See Wikipedia entry.)</p>
 
-<h2>What are tags?</h2>
+<h3>What are tags?</h3>
 <p>Tags are keywords aped to a blog post, photo or video to help users find related topics or media, either through browsing on the site or as a term to make your entry more relevant to search engines. (See hashtag andWikipedia entry.)</p>
 
 
-<h2>What are terms of service?</h2>
+<h3>What are terms of service?</h3>
 <p>Terms of service (TOS) are the legal basis upon which you agree to use a website, video hosting site or other place for creating or sharing content. Check before agreeing to concede the rights the site owners may claim over your content. (See Wikipedia entry.)</p>
 
-<h2>What is a tweet?</h2>
+<h3>What is a tweet?</h3>
 <p>A post on Twitter, a real-time social messaging system. While all agree on usage of tweet as a noun, people disagree on whether you “tweet” or “twitter” as a verb. RT stands for retweet: Users ap RT in a tweet if they are reposting something from another person’s tweet.</p>
 
-<h2>What is Twitter?</h2>
+<h3>What is Twitter?</h3>
 <p>Twitter is a popular social network, unveiled to the public in July 2006, that lets members post updates of no more than 140 characters. People have begun using Twitter in interesting ways to point to news stories, to raise funds for charity, and other unexpected uses.</p>
 
-<h2>What is a webinar?</h2>
+<h3>What is a webinar?</h3>
 <p>Short for Web-based seminar, a webinar is a presentation, lecture, workshop or seminar that is transmitted over the Web. In general, participants register in advance and access the presentation in real time over the Internet and listen to the presenter either through computer speakers or a telephone connection. Webinars are generally one-way and can involve chat or polls. There are a large number of companies that offer webinar services. (See Wikipedia entry.)</p>
 
-<h2>What is a widget?</h2>
+<h3>What is a widget?</h3>
 <p>A widget, sometimes called a gadget, badge or applet, is a small block of content, typically displayed in a small box, with a specific purpose, such as providing weather forecasts or news, that is constantly updating itself (typically via RSS). Widgets make it easy to ap dynamic content to your site or blog. (See Wikipedia entry.)</p>
--- website/application/views/pages/terms_of_use_view.php
+++ website/application/views/pages/terms_of_use_view.php
@@ -1,7 +1,7 @@
 <h1>Terms of Service</h1>
 <p>These Terms of Service ("Terms") govern your access to and use of the Voting Information Project social media dashboard ("ElectionDesk") sponsored by the The Pew Charitable Trusts (“Pew”). Your access to and use of the Services are conditioned on your acceptance of and compliance with these Terms. By accessing or using the Services you agree to be bound by these Terms.</p>
 
-<h2>1. Basic Terms</h2>
+<h3>1. Basic Terms</h3>
 
 <p>You are responsible for your use of the ElectionDesk, for any Content you post from the Services, and for any consequences thereof. The Content you submit, post, or display will be able to be viewed through connected third party services and websites. You should only provide Content that you are comfortable sharing with others under these Terms.</p>
 
@@ -10,15 +10,15 @@
 <p>The ElectionDesk that Pew provides is always evolving and the form and nature of the ElectionDesk that Pew provides may change from time to time without prior notice to you. In addition, Pew may stop (permanently or temporarily) providing the Services (or any features within the Services) to you or to users generally and may not be able to provide you with prior notice. We also retain the right to create limits on use and storage at our sole discretion at any time without prior notice to you.</p>
 
 
-<h2>2. Privacy</h2>
+<h3>2. Privacy</h3>
 <p>Any information that you provide to ElectionDesk is subject to our Privacy Policy, which governs our collection and use of your information. You understand that through your use of ElectionDesk you consent to the collection and use (as set forth in the Privacy Policy) of this information, including the transfer of this information to the United States and/or other countries for storage, processing and use by Pew. As part of providing you ElectionDesk, we may need to provide you with certain communications, such as service announcements and administrative messages. These communications are considered part of ElectionDesk, which you may not be able to opt-out from receiving.</p>
-<h2>3. Passwords</h2>
+<h3>3. Passwords</h3>
 <p>You are responsible for safeguarding the password that you use to access ElectionDesk and for any activities or actions under your password. We encourage you to use "strong" passwords (passwords that use a combination of upper and lower case letters, numbers and symbols) with your account. Pew cannot and will not be liable for any loss or damage arising from your failure to comply with the above.</p>
-<h2>4. Content on the Services</h2>
+<h3>4. Content on the Services</h3>
 <p>All Content, whether publicly posted or privately transmitted, is the sole responsibility of the person who originated such Content. We may not monitor or control the Content posted via ElectionDesk and, we cannot take responsibility for such Content. Any use or reliance on any Content or materials posted via ElectionDesk or obtained by you through ElectionDesk is at your own risk.</p>
 
 <p>We do not endorse, support, represent or guarantee the completeness, truthfulness, accuracy, or reliability of any Content or communications posted via ElectionDesk or endorse any opinions expressed via ElectionDesk. You understand that by using ElectionDesk, you may be exposed to Content that might be offensive, harmful, inaccurate or otherwise inappropriate, or in some cases, postings that have been mislabeled or are otherwise deceptive. Under no circumstances will Pew be liable in any way for any Content, including, but not limited to, any errors or omissions in any Content, or any loss or damage of any kind incurred as a result of the use of any Content posted, emailed, transmitted or otherwise made available via ElectionDesk or broadcast elsewhere.</p>
-<h2>5. Your Rights</h2>
+<h3>5. Your Rights</h3>
 <p>You retain your rights to any Content you submit, post or display on or through ElectionDesk. By submitting, posting or displaying Content on or through ElectionDesk, you grant us a worldwide, non-exclusive, royalty-free license (with the right to sublicense) to use, copy, reproduce, process, adapt, modify, publish, transmit, display and distribute such Content in any and all media or distribution methods (now known or later developed).</p>
 
 <p>You agree that this license includes the right for Pew to provide, promote, and improve ElectionDesk and to make Content submitted to or through ElectionDesk available to other companies, organizations or individuals who partner with Pew for the syndication, broadcast, distribution or publication of such Content on other media and services, subject to our terms and conditions for such Content use.</p>
@@ -29,18 +29,18 @@
 
 <p>You are responsible for your use of ElectionDesk, for any Content you provide, and for any consequences thereof, including the use of your Content by other users and our third party partners. You understand that your Content may be syndicated, broadcast, distributed, or published by our partners and if you do not have the right to submit Content for such use, it may subject you to liability. Pew will not be responsible or liable for any use of your Content by Pew in accordance with these Terms. You represent and warrant that you have all the rights, power and authority necessary to grant the rights granted herein to any Content that you submit.</p>
 
-<h2>6. Your License To Use Election Desk</h2>
+<h3>6. Your License To Use Election Desk</h3>
 <p>Pew gives you a personal, worldwide, royalty-free, non-assignable and non-exclusive license to use the software that is provided to you by Pew as part of the ElectionDesk. This license is for the sole purpose of enabling you to use and enjoy the benefit of ElectionDesk as provided by Pew, in the manner permitted by these Terms.</p>
-<h2>7. Pew's Rights</h2>
+<h3>7. Pew's Rights</h3>
 <p>All right, title, and interest in and to ElectionDesk (excluding Content provided by users) are and will remain the exclusive property of Pew and its licensors. ElectionDesk is protected by copyright, trademark, and other laws of both the United States and foreign countries. Nothing in the Terms gives you a right to use the Pew name or any of the Pew trademarks, logos, domain names, and other distinctive brand features. Any feedback, comments, or suggestions you may provide regarding Pew or ElectionDesk is entirely voluntary, and we will be free to use such feedback, comments or suggestions as we see fit and without any obligation to you.</p>
-<h2>8. Restrictions on Content and Use of ElectionDesk</h2>
+<h3>8. Restrictions on Content and Use of ElectionDesk</h3>
 <p>Please review the Rules (which are part of these Terms) to better understand what is prohibited on the ElectionDesk. We reserve the right at all times (but will not have an obligation) to remove or refuse to distribute any Content on ElectionDesk, to suspend or terminate users, and to reclaim usernames without liability to you. We also reserve the right to access, read, preserve, and disclose any information as we reasonably believe is necessary to (i) satisfy any applicable law, regulation, legal process or governmental request, (ii) enforce the Terms, including investigation of potential violations hereof, (iii) detect, prevent, or otherwise address fraud, security or technical issues, (iv) respond to user support requests, or (v) protect the rights, property or safety of Pew, its users and the public.</p>
 
 <p>Except as permitted through ElectionDesk, these Terms, or the terms provided on dev.twitter.com, you have to use the Twitter API if you want to reproduce, modify, create derivative works, distribute, sell, transfer, publicly display, publicly perform, transmit, or otherwise use the Content or Services.</p>
 
 <p>You may not do any of the following while accessing or using ElectionDesk: (i) access, tamper with, or use non-public areas of ElectionDesk, Pew’s computer systems, or the technical delivery systems of Pew’s providers; (ii) probe, scan, or test the vulnerability of any system or network or breach or circumvent any security or authentication measures; (iii) access or search or attempt to access or search ElectionDesk by any means (automated or otherwise) other than through our currently available, published interfaces that are provided by Pew (and only pursuant to those terms and conditions), unless you have been specifically allowed to do so in a separate agreement with Pew (NOTE: crawling ElectionDesk is permissible if done in accordance with the provisions of the robots.txt file, however, scraping ElectionDesk without the prior consent of Pew is expressly prohibited); (iv) forge any TCP/IP packet header or any part of the header information in any email or posting, or in any way use ElectionDesk to send altered, deceptive or false source-identifying information; or (v) interfere with, or disrupt, (or attempt to do so), the access of any user, host or network, including, without limitation, sending a virus, overloading, flooding, spamming, mail-bombing the ElectionDesk or by scripting the creation of Content in such a manner as to interfere with or create an undue burden on the ElectionDesk.</p>
 
-<h2>9. Copyright Policy</h2>
+<h3>9. Copyright Policy</h3>
 <p>Pew respects the intellectual property rights of others and expects users of ElectionDesk to do the same. We will respond to notices of alleged copyright infringement that comply with applicable law and are properly provided to us. If you believe that your Content has been copied in a way that constitutes copyright infringement, please provide us with the following information: (i) a physical or electronic signature of the copyright owner or a person authorized to act on their behalf; (ii) identification of the copyrighted work claimed to have been infringed; (iii) identification of the material that is claimed to be infringing or to be the subject of infringing activity and that is to be removed or access to which is to be disabled, and information reasonably sufficient to permit us to locate the material; (iv) your contact information, including your address, telephone number, and an email address; (v) a statement by you that you have a good faith belief that use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law; and (vi) a statement that the information in the notification is accurate, and, under penalty of perjury, that you are authorized to act on behalf of the copyright owner.</p>
 
 <p>We reserve the right to remove Content alleged to be infringing without prior notice, at our sole discretion, and without liability to you. In appropriate circumstances, Pew will also terminate a user’s account if the user is determined to be a repeat infringer. Our designated copyright agent for notice of alleged copyright infringement appearing on ElectionDesk is:<p>
@@ -53,7 +53,7 @@
 </address>
 <p>Email: <a href="mail:copyright@twitter.com">copyright@twitter.com</a></p>
 
-<h2>10. Ending These Terms</h2>
+<h3>10. Ending These Terms</h3>
 <p>The Terms will continue to apply until terminated by either you or Pew as follows.</p>
 
 <p>You may end your legal agreement with Pew at any time for any reason by deactivating your accounts and discontinuing your use of ElectionDesk. You do not need to specifically inform Pew when you stop using ElectionDesk. If you stop using ElectionDesk without deactivating your accounts, your accounts may be deactivated due to prolonged inactivity under our Inactive Account Policy.</p>
@@ -64,30 +64,30 @@
 
 <p>Nothing in this section shall affect Pew’s rights to change, limit or stop the provision of ElectionDesk without prior notice, as provided above in section 1.</p>
 
-<h2>11.Disclaimers and Limitations of Liability</h2>
+<h3>11.Disclaimers and Limitations of Liability</h3>
 <p>Please read this section carefully since it limits the liability of Pew and its parents, subsidiaries, affiliates, related companies, officers, directors, employees, agents, representatives, partners, and licensors (collectively, the “Twitter Entities”). Each of the subsections below only applies up to the maximum extent permitted under applicable law. Some jurisdictions do not allow the disclaimer of implied warranties or the limitation of liability in contracts, and as a result the contents of this section may not apply to you. Nothing in this section is intended to limit any rights you may have which may not be lawfully limited.</p>
 
-<h3>A. The Services are Available "AS-IS"</h3>
+<h4>A. The Services are Available "AS-IS"</h4>
 <p>Your access to and use of ElectionDesk or any Content are at your own risk. You understand and agree that ElectionDesk is provided to you on an "AS IS" and "AS AVAILABLE" basis. Without limiting the foregoing, to the maximum extent permitted under applicable law, THE TWITTER ENTITIES DISCLAIM ALL WARRANTIES AND CONDITIONS, WHETHER EXPRESS OR IMPLIED, OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.</p>
 
 <p>Pew makes no warranty and disclaims all responsibility and liability for: (i) the completeness, accuracy, availability, timeliness, security or reliability of ElectionDesk or any Content; (ii) any harm to your computer system, loss of data, or other harm that results from your access to or use of ElectionDesk or any Content; (iii) the deletion of, or the failure to store or to transmit, any Content and other communications maintained by ElectionDesk; and (iv) whether ElectionDesk will meet your requirements or be available on an uninterrupted, secure, or error-free basis. No advice or information, whether oral or written, obtained from the Twitter Entities or through the ElectionDesk, will create any warranty not expressly made herein.</p>
-<h3>B. Links</h3>
+<h4>B. Links</h4>
 <p>ElectionDesk may contain links to third-party websites or resources. You acknowledge and agree that the Twitter Entities are not responsible or liable for: (i) the availability or accuracy of such websites or resources; or (ii) the content, products, or services on or available from such websites or resources. Links to such websites or resources do not imply any endorsement by the Twitter Entities of such websites or resources or the content, products, or services available from such websites or resources. You acknowledge sole responsibility for and assume all risk arising from your use of any such websites or resources.</p>
-<h3>C. Limitation of Liability</h3>
+<h4>C. Limitation of Liability</h4>
 <p>TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, THE TWITTER ENTITIES SHALL NOT BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL OR PUNITIVE DAMAGES, OR ANY LOSS OF PROFITS OR REVENUES, WHETHER INCURRED DIRECTLY OR INDIRECTLY, OR ANY LOSS OF DATA, USE, GOOD-WILL, OR OTHER INTANGIBLE LOSSES, RESULTING FROM (i) YOUR ACCESS TO OR USE OF OR INABILITY TO ACCESS OR USE ELECTIONDESK; (ii) ANY CONDUCT OR CONTENT OF ANY THIRD PARTY ON ELECTIONDESK, INCLUDING WITHOUT LIMITATION, ANY DEFAMATORY, OFFENSIVE OR ILLEGAL CONDUCT OF OTHER USERS OR THIRD PARTIES; (iii) ANY CONTENT OBTAINED FROM ELECTIONDESK; OR (iv) UNAUTHORIZED ACCESS, USE OR ALTERATION OF YOUR TRANSMISSIONS OR CONTENT.</p>
 
 <p>IN NO EVENT SHALL THE AGGREGATE LIABILITY OF THE TWITTER ENTITIES EXCEED THE GREATER OF ONE HUNDRED U.S. DOLLARS (U.S. $100.00) OR THE AMOUNT YOU PAID TWITTER, IF ANY, IN THE PAST SIX MONTHS FOR ELECTIONDESK GIVING RISE TO THE CLAIM.</p>
 
 <p>THE LIMITATIONS OF THIS SUBSECTION SHALL APPLY TO ANY THEORY OF LIABILITY, WHETHER BASED ON WARRANTY, CONTRACT, STATUTE, TORT (INCLUDING NEGLIGENCE) OR OTHERWISE, AND WHETHER OR NOT THE TWITTER ENTITIES HAVE BEEN INFORMED OF THE POSSIBILITY OF ANY SUCH DAMAGE, AND EVEN IF A REMEDY SET FORTH HEREIN IS FOUND TO HAVE FAILED OF ITS ESSENTIAL PURPOSE.</p>
 
-<h2>12. General Terms</h2>
-<h3>A. Waiver and Severability</h3>
+<h3>12. General Terms</h3>
+<h4>A. Waiver and Severability</h4>
 <p>The failure of Pew to enforce any right or provision of these Terms will not be deemed a waiver of such right or provision. In the event that any provision of these Terms is held to be invalid or unenforceable, then that provision will be limited or eliminated to the minimum extent necessary, and the remaining provisions of these Terms will remain in full force and effect.</p>
-<h3>B. Controlling Law and Jurisdiction</h3>
+<h4>B. Controlling Law and Jurisdiction</h4>
 <p>These Terms and any action related thereto will be governed by the laws of the State of California without regard to or application of its conflict of law provisions or your state or country of residence. All claims, legal proceedings or litigation arising in connection with ElectionDesk will be brought solely in the federal or state courts located in the District of Columbia, United States, and you consent to the jurisdiction of and venue in such courts and waive any objection as to inconvenient forum.<p>
 
 <p>If you are a federal, state, or local government entity in the United States using ElectionDesk in your official capacity and legally unable to accept the controlling law, jurisdiction or venue clauses above, then those clauses do not apply to you. For such U.S. federal government entities, these Terms and any action related thereto will be governed by the laws of the United States of America (without reference to conflict of laws) and, in the absence of federal law and to the extent permitted under federal law, the laws of the State of California (excluding choice of law).</p>
-<h3>C. Entire Agreement</h3>
+<h4>C. Entire Agreement</h4>
 <p>These Terms, the Twitter Rules and our Privacy Policy are the entire and exclusive agreement between Twitter and you regarding ElectionDesk (excluding any services for which you have a separate agreement with Pew that is explicitly in addition or in place of these Terms), and these Terms supersede and replace any prior agreements between Pew and you regarding ElectionDesk. Other than members of the group of companies of which Pew is the parent, no other person or company will be third party beneficiaries to the Terms.</p>
 
 <p>We may revise these Terms from time to time, the most current version will always be at twitter.com/tos. If the revision, in our sole discretion, is material we will notify you via an @Twitter update or e-mail to the email associated with your account. By continuing to access or use ElectionDesk after those revisions become effective, you agree to be bound by the revised Terms.</p>
