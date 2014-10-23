52d1578045907cb0d6190d91b2d9bd65face0e6a
Mathias Hansen <me@codemonkey.io>
2014-10-23 10:16:44 -0400

    Adjusted Google+ Consumer for API Limits

--- consumer/lib/Consumer/IndividualConsumer/GoogleConsumer.php
+++ consumer/lib/Consumer/IndividualConsumer/GoogleConsumer.php
@@ -3,6 +3,13 @@
 use Consumer\Log;
 
 class GoogleConsumer extends IndividualConsumer {
+	/*
+	 API Limits
+	 50,000 requests per day
+	 Which is 2,080 requests per hour
+	 We have 5 active filters, so we have 416 requests per hour per filter
+	 Each filter can have up to 6 pages, so each filter can run 69 times per hour
+	*/
 
 	private $maxPages = 6;
 
@@ -60,6 +67,6 @@ class GoogleConsumer extends IndividualConsumer {
 	        }
 	    }
 
-	    sleep(60);
+	    sleep(50); // Slow things down so we don't hit API limits
 	}	
 }
